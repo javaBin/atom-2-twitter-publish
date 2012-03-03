03bd85ba9e77ca8ddfa1c77a105bcc8b71cba29c
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-03-03 10:19:58 +0100

    Added sonatype publish

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import sbtrelease.ReleaseKeys._
 object Atom2TwitterPublish extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ releaseSettings ++ Seq(
-    organization := "no.javabin",
+    organization := "no.java",
     scalaVersion := "2.9.1",
     releaseProcess <<= thisProjectRef apply { ref =>
       import sbtrelease.ReleaseStateTransformations._
@@ -24,8 +24,39 @@ object Atom2TwitterPublish extends Build {
         setNextVersion,
         commitNextVersion
       )
-    }
+    },
+   credentials += Credentials(Path.userHome / ".sbt" / ".credentials"),
+   publishSetting
+  ) ++ mavenCentralFrouFrou
+
+  lazy val publishSetting = publishTo <<= (version) { version: String =>
+    if (version.trim.endsWith("SNAPSHOT"))
+      Some(Resolvers.sonatypeNexusSnapshots)
+    else
+      Some(Resolvers.sonatypeNexusStaging)
+  }
+
+  // Things we care about primarily because Maven Central demands them
+  lazy val mavenCentralFrouFrou = Seq(
+    homepage := Some(new URL("http://wiki.java.no")),
+    startYear := Some(2010),
+    licenses := Seq(("Apache 2", new URL("http://www.apache.org/licenses/LICENSE-2.0.txt"))),
+    pomExtra <<= (pomExtra, name, description) {(pom, name, desc) => pom ++ xml.Group(
+      <scm>
+        <url>https://github.com/javaBin/atom-2-twitter-publish</url>
+        <connection>scm:git:git://github.com/javaBin/atom-2-twitter-publish.git</connection>
+        <developerConnection>scm:git:git@github.com:javaBin/atom-2-twitter-publish.git</developerConnection>
+      </scm>
+      <developers>
+        <developer>
+          <id>trygvis</id>
+          <name>Trygve Laugstøl</name>
+          <url>http://twitter.com/trygvis</url>
+        </developer>
+      </developers>
+    )}
   )
+
 
   val dispatchVersion = "0.7.8"
   val dispatchTwitter = "net.databinder" %% "dispatch-twitter" % dispatchVersion withSources
@@ -49,5 +80,10 @@ object Atom2TwitterPublish extends Build {
         specs
       )
     )
-  )
+  ) 
+}
+
+object Resolvers {
+  val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  val sonatypeNexusStaging = "Sonatype Nexus Staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
 }
